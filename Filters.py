d0349f3d2159a776fc4a294fac3669e2596bde33
Thomas Vanesse <vanessethomas@gmail.com>
2014-03-28 15:25:39 +0100

    Unit test for Filters.py.

--- Filters.py
+++ Filters.py
@@ -5,10 +5,14 @@ class Filter(object):
 	An abstract object that is able to produce a filtered version of a CSVFile based
 	on some filtering criterias.
 	"""
-	def __init__(self, csvin=CSVFile()):
+	def __init__(self, csvin):
 		self._input = csvin
 		
-	def job(self):
+	@property
+	def inputCSV(self):		# damn it ! input is already used by Python
+		return self._input
+		
+	def output(self):
 		"""
 		This method is abstract. It should be overriden by all the classes extending
 		Filter.
@@ -20,11 +24,11 @@ class FieldSelector(Filter):
 	"""
 	A Filter object that selects a field within a given CSVFile.
 	"""
-	def __init__(self, csvin=CSVFile(), field):
-		super(FieldSelector, self).__init__(csvin)
+	def __init__(self, field, csvin):
+		super(FieldSelector, self).__init__(csvin=csvin)
 		self._field = field
 	
-	def job(self):
+	def output(self):
 		"""
 		Returns a Field object out of a bigger CSVFile.
 		"""
@@ -44,3 +48,16 @@ class Remover(Filter):
 	
 class Inserter(Filter):
 	pass
+	
+	
+# ----- TESTING ----- #
+if __name__ == "__main__" :
+	import sys
+	import os
+	if os.path.isfile(os.path.abspath(sys.argv[1])):
+		csv_file    = CSVFile(os.path.abspath(sys.argv[1]))
+		filter_test = FieldSelector(csvin=csv_file, field="X_CURRENT_TARGET_SP")
+		print(csv_file)
+		print(filter_test.output())
+	else:
+		print("Error, the file does not exist")

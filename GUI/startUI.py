eb64cdd4668e9223ad434a983dd99260534b225f
Thomas Vanesse <vanessethomas@gmail.com>
2014-03-31 02:52:59 +0200

    GUI improvements. The drag and drop from the notepad to the workflow does not work.

--- GUI/startUI.py
+++ GUI/startUI.py
@@ -9,10 +9,6 @@ Reminder : how the Builder works
 --------------------------------
 The Gtk.Builder class offers you the opportunity to design user interfaces without writing a single line of code. This is possible through describing the interface by a XML file (done via Glade) and then loading the XML description at runtime and create the objects automatically, which the Builder class does for you.
 """
-# Trick to allow relative paths
-#file_path = os.path.abspath(os.path.dirname(sys.argv[0]))
-#os.chdir(file_path)
-
 builder = Gtk.Builder()
 builder.add_from_file("theforce/GUI/main.glade")
 
@@ -30,12 +26,37 @@ scrolledwindow_display = builder.get_object("scrolledwindow_display")
 # Information panel
 default_info_label = builder.get_object("default_info_label")
 
+# -----------------
+# Filters iconview
+# -----------------
+filters_iconview = builder.get_object("filters_iconview")
+filters_liststore = builder.get_object("filters_liststore")
+filters_iconview.set_pixbuf_column(1)
+filters_iconview.set_text_column(0)
+# The different filters
+filters_liststore.append(["Field selector", Gtk.IconTheme.get_default().load_icon("gtk-copy", 32, 0)])
+# Drag and drop behaviour. This is the source.
+filters_iconview.enable_model_drag_source(Gdk.ModifierType.BUTTON1_MASK, [], Gdk.DragAction.COPY)
+
+# -----------------
+# Workflow grid
+# -----------------
+workflow_grid = builder.get_object("workflow_grid")
+workflow_grid.drag_dest_set(Gtk.DestDefaults.ALL, [], Gdk.DragAction.COPY)
+first_frame_label = builder.get_object("first_frame_label")
+first_frame_label.drag_dest_set(Gtk.DestDefaults.ALL, [], Gdk.DragAction.COPY)
+
+
 # Signal handlers
 class Signal_Handlers:
 	def on_main_window_delete_event(self, *args):
 		Gtk.main_quit(*args)
 		
 	def on_filechooserbutton_file_set(self, widget):
+		# Destroy the yoda head
+		builder.get_object("yoda_viewport").destroy()
+		
+		# Load the file
 		csv_file = CSVFile(widget.get_filename())
 		
 		# Create a grid representation of the csv
@@ -53,6 +74,18 @@ class Signal_Handlers:
 				row_no += 1
 				grid.attach(Gtk.Label(str(d)), left=col_no, top=row_no, width=1, height=1)
 		scrolledwindow_display.show_all()
+		
+	def on_filters_iconview_drag_begin(self, widget, drag_context):
+		print("Drag started.")
+		
+	def on_filters_iconview_drag_data_get(self, widget, drag_context):
+		print("Drag launched.")
+		
+	def on_workflow_drag_data_received(self, widget, drag_context, x,y, data,info, time):
+		print("Drop")
+		
+	def on_workflow_drag_motion(self, widget, drag_context):
+		print("Fly me to the moon")
 		
 builder.connect_signals(Signal_Handlers())
 main_window.show_all()
